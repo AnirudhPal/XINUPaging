e2ce6d9a201a60e8ded3e4267b90dbe800459512
AnirudhPal <pal5@purdue.edu>
2018-12-06 21:22:36 -0500

    Fix Error

--- xinu-fall2018-paging/system/createVApps.c
+++ xinu-fall2018-paging/system/createVApps.c
@@ -35,7 +35,7 @@ void testPP(unsigned int pg) {
 	// Print
 	mask = disable();
 	kprintf("PID: %d -> Used All Pages, First Add: 0x%x, First Val: 0x%x, pfCount: %d\n", currpid, ptr, *(ptr), get_faults());
-	kprintf("PID: %d -> Used All Pages, Last Add: 0x%x, First Val: 0x%x, pfCount: %d\n", currpid, ptr + (pg * NBPG), *(ptr + (pg * NBPG)), get_faults());
+	kprintf("PID: %d -> Used All Pages, Last Add: 0x%x, First Val: 0x%x, pfCount: %d\n", currpid, ptr + (pg * NBPG) - 1, *(ptr + (pg * NBPG) - 1, get_faults());
 	restore(mask);
 }
 
